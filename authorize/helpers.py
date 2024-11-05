15146e52fb57fbb7ab8bf1124cf052ff9340bdb8
Melika <melika.akz97@gmail.com>
2024-11-05 14:32:28 +0330

    added swager

--- authorize/helpers.py
+++ authorize/helpers.py
@@ -1,4 +1,4 @@
-from datetime import timedelta, datetime
+from datetime import timedelta, datetime, UTC
 
 import jwt
 from sparrow.settings import SECRET_KEY
@@ -7,7 +7,7 @@ from sparrow.settings import SECRET_KEY
 def generate_jwt_token(user_id):
     payload = {
         'user_id': user_id,
-        'exp': datetime.utcnow() + timedelta(days=1)
+        'exp': datetime.now(UTC) + timedelta(days=1)
     }
     secret_key = SECRET_KEY
     token = jwt.encode(payload, secret_key, algorithm='HS256')
