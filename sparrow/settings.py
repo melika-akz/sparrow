40c3d726e5851f5d759c9b522445d9bb2763c45d
Melika <melika.akz97@gmail.com>
2025-05-06 11:39:57 +0330

    fix requirments

--- sparrow/settings.py
+++ sparrow/settings.py
@@ -199,7 +199,7 @@ CHANNEL_LAYERS = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",  # Port va DB ID ro bar asas config Redis taghir bede
+        "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
